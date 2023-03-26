31daf640d4413d96aa614af11f7df995d2ef4b36
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2023-03-26 14:34:43 +0200

    Add copyright header to files

--- MacOSHash.h
+++ MacOSHash.h
@@ -1,3 +1,21 @@
+/*
+ * HashSum
+ * 
+ * Copyright (c) 2023 chatgptdev
+ * 
+ * This software was written mostly by ChatGPT 4.0 using instructions by
+ * @chatgptdev. It is provided under the Apache License, Version 2.0 
+ * (the "License"); you may not use this software except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef MACOS_HASH_H
 #define MACOS_HASH_H
