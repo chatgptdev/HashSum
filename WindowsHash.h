6028d11364b2277e91bf31838946aac47bc4584b
chatgptdev <128300865+chatgptdev@users.noreply.github.com>
2023-03-26 01:15:19 +0100

    change signature of WindowsHash::Update method

--- WindowsHash.h
+++ WindowsHash.h
@@ -12,7 +12,7 @@ public:
     ~WindowsHash();
 
     bool Init(const std::string& algorithm);
-    bool Update(const std::vector<unsigned char>& data);
+    bool Update(const unsigned char* data, size_t dataSize);
     bool Final(std::vector<unsigned char>& digest);
 
     static std::vector<std::string> GetSupportedAlgorithms();
